a0866f74ef70f2ec662eb989acca2b3b5717f67d
Sebastián Kisser <kisserseba@gmail.com>
2026-03-30 13:42:23 -0300

    Ajustes realizados: - Se ajustaron los margenes de acuerdo al pdf adjuntado.

--- src/config/pdfMappingAuto.ts
+++ src/config/pdfMappingAuto.ts
@@ -19,100 +19,100 @@ const generateMirror = (items: PDFMapItem[]): PDFMapItem[] => {
 const BASE_AUTO_MAPPING: PDFMapItem[] = [
   // ----------- PÁGINA 1: FRENTE ORIGINAL (0) -----------
   // SECCIÓN A: LUGAR Y FECHA
-  { field: 'lugar', page: 0, x: 225, y: 726 },
-  { field: 'fecha', page: 0, x: 325, y: 726 },
-  { field: 'precio', page: 0, x: 205, y: 704 },
-  { field: 'vehiculo.dominio', page: 0, x: 331, y: 704 },
+  { field: 'lugar', page: 0, x: 185, y: 728 },
+  { field: 'fecha', page: 0, x: 285, y: 728 },
+  { field: 'precio', page: 0, x: 145, y: 706 },
+  { field: 'vehiculo.dominio', page: 0, x: 291, y: 706 },
 
   // SECCIÓN D: COMPRADOR
-  { field: 'comprador.nombre', page: 0, x: 210, y: 637 },
-  { field: 'comprador_condominio.nombre', page: 0, x: 450, y: 631 },
+  { field: 'comprador.nombre', page: 0, x: 150, y: 639 },
+  { field: 'comprador_condominio.nombre', page: 0, x: 390, y: 633 },
 
-  { field: 'comprador.email', page: 0, x: 210, y: 604 },
-  { field: 'comprador_condominio.email', page: 0, x: 424, y: 600 },
+  { field: 'comprador.email', page: 0, x: 160, y: 606 },
+  { field: 'comprador_condominio.email', page: 0, x: 376, y: 604 },
 
-  { field: 'comprador.telefono', page: 0, x: 210, y: 590 },
-  { field: 'comprador_condominio.telefono', page: 0, x: 428, y: 589 },
+  { field: 'comprador.telefono', page: 0, x: 170, y: 592 },
+  { field: 'comprador_condominio.telefono', page: 0, x: 388, y: 592 },
 
-  { field: 'comprador.domicilio_legal', page: 0, x: 230, y: 576 },
-  { field: 'comprador_condominio.domicilio_legal', page: 0, x: 470, y: 576 },
+  { field: 'comprador.domicilio_legal', page: 0, x: 190, y: 578 },
+  { field: 'comprador_condominio.domicilio_legal', page: 0, x: 420, y: 579 },
 
-  { field: 'comprador.domicilio_legal_numero', page: 0, x: 185, y: 557 },
-  { field: 'comprador_condominio.domicilio_legal_numero', page: 0, x: 408, y: 557 },
-  { field: 'comprador.domicilio_legal_piso', page: 0, x: 245, y: 557 },
-  { field: 'comprador_condominio.domicilio_legal_piso', page: 0, x: 468, y: 557 },
-  { field: 'comprador.domicilio_legal_depto', page: 0, x: 291, y: 557 },
-  { field: 'comprador_condominio.domicilio_legal_depto', page: 0, x: 511, y: 557 },
-  { field: 'comprador.domicilio_legal_cp', page: 0, x: 327, y: 557 },
-  { field: 'comprador_condominio.domicilio_legal_cp', page: 0, x: 544, y: 557 },
+  { field: 'comprador.domicilio_legal_numero', page: 0, x: 142, y: 559 },
+  { field: 'comprador_condominio.domicilio_legal_numero', page: 0, x: 363, y: 561 },
+  { field: 'comprador.domicilio_legal_piso', page: 0, x: 193, y: 559 },
+  { field: 'comprador_condominio.domicilio_legal_piso', page: 0, x: 413, y: 561 },
+  { field: 'comprador.domicilio_legal_depto', page: 0, x: 248, y: 559 },
+  { field: 'comprador_condominio.domicilio_legal_depto', page: 0, x: 465, y: 561 },
+  { field: 'comprador.domicilio_legal_cp', page: 0, x: 284, y: 559 },
+  { field: 'comprador_condominio.domicilio_legal_cp', page: 0, x: 504, y: 561 },
 
-  { field: 'comprador.domicilio_legal_localidad', page: 0, x: 210, y: 537 },
-  { field: 'comprador_condominio.domicilio_legal_localidad', page: 0, x: 450, y: 537 },
+  { field: 'comprador.domicilio_legal_localidad', page: 0, x: 180, y: 539 },
+  { field: 'comprador_condominio.domicilio_legal_localidad', page: 0, x: 410, y: 540 },
 
-  { field: 'comprador.domicilio_departamento_o_partido', page: 0, x: 210, y: 521 },
-  { field: 'comprador_condominio.domicilio_departamento_o_partido', page: 0, x: 430, y: 521 },
-  { field: 'comprador.domicilio_provincia', page: 0, x: 320, y: 521 },
-  { field: 'comprador_condominio.domicilio_provincia', page: 0, x: 540, y: 521 },
+  { field: 'comprador.domicilio_departamento_o_partido', page: 0, x: 145, y: 524 },
+  { field: 'comprador_condominio.domicilio_departamento_o_partido', page: 0, x: 355, y: 525 },
+  { field: 'comprador.domicilio_provincia', page: 0, x: 265, y: 524 },
+  { field: 'comprador_condominio.domicilio_provincia', page: 0, x: 485, y: 525 },
 
-  { field: 'comprador.domicilio_real', page: 0, x: 230, y: 503 },
-  { field: 'comprador_condominio.domicilio_real', page: 0, x: 470, y: 503 },
+  { field: 'comprador.domicilio_real', page: 0, x: 180, y: 506 },
+  { field: 'comprador_condominio.domicilio_real', page: 0, x: 410, y: 507 },
 
-  { field: 'comprador.domicilio_real_numero', page: 0, x: 185, y: 485 },
-  { field: 'comprador_condominio.domicilio_real_numero', page: 0, x: 408, y: 485 },
-  { field: 'comprador.domicilio_real_piso', page: 0, x: 245, y: 485 },
-  { field: 'comprador_condominio.domicilio_real_piso', page: 0, x: 468, y: 485 },
-  { field: 'comprador.domicilio_real_depto', page: 0, x: 291, y: 485 },
-  { field: 'comprador_condominio.domicilio_real_depto', page: 0, x: 511, y: 485 },
-  { field: 'comprador.domicilio_real_cp', page: 0, x: 327, y: 485 },
-  { field: 'comprador_condominio.domicilio_real_cp', page: 0, x: 544, y: 485 },
+  { field: 'comprador.domicilio_real_numero', page: 0, x: 142, y: 488 },
+  { field: 'comprador_condominio.domicilio_real_numero', page: 0, x: 363, y: 489 },
+  { field: 'comprador.domicilio_real_piso', page: 0, x: 193, y: 488 },
+  { field: 'comprador_condominio.domicilio_real_piso', page: 0, x: 413, y: 489 },
+  { field: 'comprador.domicilio_real_depto', page: 0, x: 248, y: 488 },
+  { field: 'comprador_condominio.domicilio_real_depto', page: 0, x: 465, y: 489 },
+  { field: 'comprador.domicilio_real_cp', page: 0, x: 284, y: 488 },
+  { field: 'comprador_condominio.domicilio_real_cp', page: 0, x: 504, y: 489 },
 
-  { field: 'comprador.domicilio_real_localidad', page: 0, x: 210, y: 465 },
-  { field: 'comprador_condominio.domicilio_real_localidad', page: 0, x: 450, y: 465 },
+  { field: 'comprador.domicilio_real_localidad', page: 0, x: 180, y: 468 },
+  { field: 'comprador_condominio.domicilio_real_localidad', page: 0, x: 410, y: 469 },
 
-  { field: 'comprador.domicilio_departamento_o_partido', page: 0, x: 210, y: 445 },
-  { field: 'comprador_condominio.domicilio_departamento_o_partido', page: 0, x: 430, y: 445 },
-  { field: 'comprador.domicilio_provincia', page: 0, x: 320, y: 445 },
-  { field: 'comprador_condominio.domicilio_provincia', page: 0, x: 540, y: 445 },
+  { field: 'comprador.domicilio_departamento_o_partido', page: 0, x: 145, y: 447 },
+  { field: 'comprador_condominio.domicilio_departamento_o_partido', page: 0, x: 365, y: 449 },
+  { field: 'comprador.domicilio_provincia', page: 0, x: 265, y: 447 },
+  { field: 'comprador_condominio.domicilio_provincia', page: 0, x: 485, y: 449 },
 
-  { field: 'comprador.dni', page: 0, x: 190, y: 376 },
-  { field: 'comprador_condominio.dni', page: 0, x: 410, y: 376 },
+  { field: 'comprador.dni', page: 0, x: 130, y: 379 },
+  { field: 'comprador_condominio.dni', page: 0, x: 350, y: 381 },
 
-  { field: 'comprador.cuit', page: 0, x: 230, y: 352 },
-  { field: 'comprador_condominio.cuit', page: 0, x: 450, y: 352 },
+  { field: 'comprador.cuit', page: 0, x: 190, y: 355 },
+  { field: 'comprador_condominio.cuit', page: 0, x: 410, y: 356 },
 
-  { field: 'comprador.lugar_nacimiento', page: 0, x: 242, y: 337 },
-  { field: 'comprador_condominio.lugar_nacimiento', page: 0, x: 462, y: 337 },
+  { field: 'comprador.lugar_nacimiento', page: 0, x: 202, y: 340 },
+  { field: 'comprador_condominio.lugar_nacimiento', page: 0, x: 422, y: 341 },
 
-
-  { field: 'comprador.nombre_conyugue', page: 0, x: 190, y: 285 },
-  { field: 'comprador_condominio.nombre_conyugue', page: 0, x: 430, y: 285 },
+  
+  { field: 'comprador.nombre_conyugue', page: 0, x: 150, y: 287 },
+  { field: 'comprador_condominio.nombre_conyugue', page: 0, x: 370, y: 287 },
 
   // SECCIÓN F: VEHÍCULO
-  { field: 'vehiculo.dominio', page: 0, x: 310, y: 120 },
-  { field: 'vehiculo.marca', page: 0, x: 210, y: 108 },
-  { field: 'vehiculo.tipo', page: 0, x: 198, y: 94 },
-  { field: 'vehiculo.modelo', page: 0, x: 210, y: 80 },
+  { field: 'vehiculo.dominio', page: 0, x: 260, y: 123 },
+  { field: 'vehiculo.marca', page: 0, x: 170, y: 110 },
+  { field: 'vehiculo.tipo', page: 0, x: 158, y: 96 },
+  { field: 'vehiculo.modelo', page: 0, x: 170, y: 82 },
 
-  { field: 'vehiculo.n_motor', page: 0, x: 670, y: 120 },
-  { field: 'vehiculo.marca', page: 0, x: 470, y: 108 },
-  { field: 'vehiculo.n_chasis', page: 0, x: 455, y: 94 },
-  { field: 'vehiculo.uso', page: 0, x: 425, y: 80 },
+  { field: 'vehiculo.n_motor', page: 0, x: 630, y: 123 },
+  { field: 'vehiculo.marca', page: 0, x: 430, y: 111 },
+  { field: 'vehiculo.n_chasis', page: 0, x: 415, y: 98 },
+  { field: 'vehiculo.uso', page: 0, x: 385, y: 84 },
 
   // ----------- PÁGINA 2: DORSO ORIGINAL (1) -----------
   // SECCIÓN I: VENDEDOR
-  { field: 'vendedor.nombre', page: 1, x: 140, y: 757 },
-  { field: 'vendedor.dni', page: 1, x: 95, y: 628 },
+  { field: 'vendedor.nombre', page: 1, x: 90, y: 765 },
+  { field: 'vendedor.dni', page: 1, x: 50, y: 629 },
 
   // SECION J: CONDOMINIO VENDEDOR (Dorso)
-  { field: 'vendedor_condominio.nombre', page: 1, x: 350, y: 757 },
-  { field: 'vendedor_condominio.dni', page: 1, x: 310, y: 628 },
+  { field: 'vendedor_condominio.nombre', page: 1, x: 300, y: 764 },
+  { field: 'vendedor_condominio.dni', page: 1, x: 260, y: 629 },
 
   // SECCION E: CONDOMINIO COMPRADOR (Frente)
-  { field: 'comprador.nombre', page: 1, x: 140, y: 335 },
-  { field: 'comprador.dni', page: 1, x: 95, y: 279 },
+  { field: 'comprador.nombre', page: 1, x: 90, y: 342 },
+  { field: 'comprador.dni', page: 1, x: 50, y: 286 },
 
-  { field: 'comprador_condominio.nombre', page: 1, x: 350, y: 335 },
-  { field: 'comprador_condominio.dni', page: 1, x: 310, y: 279 },
+  { field: 'comprador_condominio.nombre', page: 1, x: 300, y: 343 },
+  { field: 'comprador_condominio.dni', page: 1, x: 260, y: 287 },
 ];
 
 
